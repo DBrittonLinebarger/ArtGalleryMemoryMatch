cc800071d96c0f8a559161fd5dd7fdf91bfead14
Denelle Britton Linebarger <brittod80@yahoo.com>
2019-07-03 15:52:16 -0600

    added onCreate to game4x4 fragment and adjusted activity_main.xml

--- app/src/main/java/edu/cnm/deepdive/gallery_match/controller/Game4x4ActivityFragment.java
+++ app/src/main/java/edu/cnm/deepdive/gallery_match/controller/Game4x4ActivityFragment.java
@@ -4,10 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import edu.cnm.deepdive.gallery_match.R;
+import edu.cnm.deepdive.gallery_match.model.entity.Game;
 
 public class Game4x4ActivityFragment extends Fragment {
+
+  private Game game;
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    //game = new game();
+  }
 
   @Override
   public View onCreateView(LayoutInflater inflater,
