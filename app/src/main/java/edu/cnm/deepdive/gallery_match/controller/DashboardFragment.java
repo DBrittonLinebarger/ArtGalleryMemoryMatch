6535e4427809aa93e54f20ab19601b773d0cd6bc
Denelle Britton Linebarger <brittod80@yahoo.com>
2019-07-09 19:39:10 -0600

    updated game4x4 fragment

--- app/src/main/java/edu/cnm/deepdive/gallery_match/controller/DashboardFragment.java
+++ app/src/main/java/edu/cnm/deepdive/gallery_match/controller/DashboardFragment.java
@@ -1,8 +1,10 @@
 package edu.cnm.deepdive.gallery_match.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -10,13 +12,15 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 import edu.cnm.deepdive.gallery_match.R;
 import edu.cnm.deepdive.gallery_match.viewmodel.ThemeViewModel;
 
 public class DashboardFragment extends Fragment {
 
-  //private Button button4x4;
+  private Button button4x4;
 
   public static DashboardFragment newInstance(){
     return new DashboardFragment();
@@ -44,16 +48,21 @@ public class DashboardFragment extends Fragment {
     search.setOnClickListener((v) ->
         themeViewModel.getSearchResult(searchTerm.getText().toString().trim()).observe(this,
             result -> {}));
+
+
+    Button button4x4 = (Button)view.findViewById(R.id.button_4x4_game);
+
+    button4x4.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.container, Game4x4Fragment.newInstance(), null);
+        transaction.commit();
+
+      }
+    });
     return view;
-
-    //button4x4 = (Button)findViewById(R.id.button_4x4_game);
-
-    //button4x4.setOnClickListener(new OnClickListener()) {
-    //  @Override
-    //      public void onClick(View view) {
-    //    Intent intent = new Intent(DashboardFragment.this, Game4x4ActivityFragment.class);
-    //  }
-    //}
   }
 
 }
--- app/src/main/java/edu/cnm/deepdive/gallery_match/controller/Game4x4Fragment.java
+++ app/src/main/java/edu/cnm/deepdive/gallery_match/controller/Game4x4Fragment.java
@@ -9,9 +9,13 @@ import androidx.fragment.app.Fragment;
 import edu.cnm.deepdive.gallery_match.R;
 import edu.cnm.deepdive.gallery_match.model.entity.Game;
 
-public class Game4x4ActivityFragment extends Fragment {
+public class Game4x4Fragment extends Fragment {
 
   private Game game;
+
+  public static DashboardFragment newInstance(){
+    return new DashboardFragment();
+  }
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,7 +28,7 @@ public class Game4x4ActivityFragment extends Fragment {
       ViewGroup container,
       Bundle savedInstanceState) {
     //Inflate the layout for this fragment
-    View view = inflater.inflate(R.layout.fragment_game4x4activity,
+    View view = inflater.inflate(R.layout.fragment_game4x4,
         container, false);
     return view;
   }
