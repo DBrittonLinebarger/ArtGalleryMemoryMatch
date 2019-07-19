7dc89c3102900967b77481b9da05af68fc889c8f
Denelle Britton Linebarger <brittod80@yahoo.com>
2019-07-19 10:58:18 -0600

    Updated ddl and data-model md.

--- docs/ddl.sql
+++ docs/ddl.sql
@@ -9,5 +9,29 @@ CREATE TABLE IF NOT EXISTS `Card`
     FOREIGN KEY (`theme_id`) REFERENCES `Theme` (`id`) ON UPDATE NO ACTION ON DELETE CASCADE
 );
 
-CREATE INDEX `index_Card_object_id` ON `Card` (`object_id`);
+CREATE TABLE IF NOT EXISTS `Game`
+(
+    `id`           INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    `player_id`    INTEGER                           NOT NULL,
+    `theme_id`     INTEGER                           NOT NULL,
+    `play_time`    INTEGER                           NOT NULL,
+    `score`        INTEGER                           NOT NULL,
+    `date_started` INTEGER                           NOT NULL,
+    `date_ended`   INTEGER                           NOT NULL,
+    `timestamp`    INTEGER,
+    FOREIGN KEY (`theme_id`) REFERENCES `Theme` (`id`) ON UPDATE NO ACTION ON DELETE CASCADE,
+    FOREIGN KEY (`player_id`) REFERENCES `Player` (`id`) ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `Player`
+(
+    `id`   INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    `name` TEXT
+);
+
+CREATE TABLE IF NOT EXISTS `Theme`
+(
+    `id`    INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    `title` TEXT
+);
 
